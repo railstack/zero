e1bb49345fd235bb83938d13266d21b047ee33bf
B1nj0y <v@joyv.net>
2017-10-12 01:09:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Zero
+
+Zero just provides some helpers for those Gophers prefer the `zero values` than touching the `sql.Null*` types when you have to work with some database tables with nullable fields.
+
+Zero's main idea is using a function `COALESCE` that most popular databases support, as:
+
+[SQLite](https://sqlite.org/lang_corefunc.html#coalesce)
+[MySQL](https://dev.mysql.com/doc/refman/8.0/en/comparison-operators.html#function_coalesce)
+[PostgreSQL](https://www.postgresql.org/docs/current/static/functions-conditional.html#functions-coalesce-nvl-ifnull)
+
+And I first got the inspiration from [here](https://github.com/go-sql-driver/mysql/issues/34#issuecomment-158391340).
+
+## Usage
+
+Now you must have known what the function `COALESCE` does. So what `zero` does is simple, it helps you write `COALESCE` function calls.
+
+Create a sample table at first:
+
+
+Let's show how use `zero` to work with a nullable field. Firstly we see the method call style:
+
+```go
+import (
+    "fmt"
+    "github.com/goonr/zero"
+)
+
+
+func main() {
+    zr := zero.New("mysql") // or "postgres"
+    sql := fmt.Sprintf("SELECT id, name, %v FROM users", zr.Int("age"))
+    // here the "sql" will be `SELECT id, name, COALESCE(age, 0) AS age FROM users`
+    /* and then do a query with the "sql" %/
+}
+```
+
+here we create an object using the `New()` function.
+
+Of course you can call the equivalent function without creating an object:
+
+```go
+import (
+    "fmt"
+    "github.com/goonr/zero"
+)
+
+
+func main() {
+    sql := fmt.Sprintf("SELECT id, name, %v FROM users", zero.Int("age"))
+    // here the "sql" will be `SELECT id, name, COALESCE(age, 0) AS age FROM users`
+    /* and then do a query with the "sql" %/
+}
+```
+
+but the function `DateTime()` is a little different due to it's database independent, so it need to pass the database name as the first parameter:
+
+```go
+/* ... */
+zero.Time("mysql", "FEILD_NAME")
+/* ... */
+```
+
+## Functions avaliable
+
+* `String()` is for string type variables in Go
+* `Int()` is for all the `int*` typed variables in Go
+* `Float()` is for all the `float*` typed variables
+* `Bool()` is for the `bool` typed variables
+* `Time()` is for the `time.Time` typed variables
+
+
+And for each of above there's a correnponding `TypeAs()` function that take another parameter as a `AS` alias name, for example:
+
+```go
+zero.StringAs("name", "last_name") // will return: COALESCE(name, "") AS last_name
+```
+
+
---
+++ zero.go
@@ -0,0 +1,104 @@
+package zero
+
+import (
+	"fmt"
+)
+
+var supportedDatabases = []string{"sqlite", "mysql", "postgres"}
+
+type Nullable struct {
+	Database string
+}
+
+func New(database string) *Nullable {
+	isSupported := false
+	for _, db := range supportedDatabases {
+		if database == db {
+			isSupported = true
+		}
+	}
+	if !isSupported {
+		panic("not supported database")
+	}
+	return &Nullable{Database: database}
+}
+
+func (n *Nullable) String(field string) string {
+	return StringAs(field, field)
+}
+
+func (n *Nullable) Int(field string) string {
+	return IntAs(field, field)
+}
+
+func (n *Nullable) Float(field string) string {
+	return FloatAs(field, field)
+}
+
+func (n *Nullable) Bool(field string) string {
+	return BoolAs(field, field)
+}
+
+func (n *Nullable) Time(field string) string {
+	return TimeAs(n.Database, field, field)
+}
+
+// String return converted SQL chunk for a nullable string typed field,
+// here the parameter "field" stands for field name
+func String(field string) string {
+	return StringAs(field, field)
+}
+
+// Int return converted SQL chunk for a nullable int* typed field
+func Int(field string) string {
+	return FloatAs(field, field)
+}
+
+// Float return converted SQL chunk for a nullable float* typed field
+func Float(field string) string {
+	return FloatAs(field, field)
+}
+
+// Bool return converted SQL chunk for a nullable boolean typed field
+func Bool(field string) string {
+	return BoolAs(field, field)
+}
+
+// Time return converted SQL chunk for a nullable datetime typed field,
+// and this function is database dependent
+func Time(database, field string) string {
+	return TimeAs(database, field, field)
+}
+
+// StringAs return converted SQL chunk for a nullable string typed field
+// here the parameter "field" stands for field name
+func StringAs(field, as string) string {
+	return fmt.Sprintf("COALESCE(%v, '') AS %v", field, as)
+}
+
+// NullIntAs return converted SQL chunk for a nullable int* typed field
+func IntAs(field, as string) string {
+	return fmt.Sprintf("COALESCE(%v, 0) AS %v", field, as)
+}
+
+// FloatAs return converted SQL chunk for a nullable float* typed field
+func FloatAs(field, as string) string {
+	return fmt.Sprintf("COALESCE(%v, 0.0) AS %v", field, as)
+}
+
+// BoolAs return converted SQL chunk for a nullable boolean typed field
+func BoolAs(field, as string) string {
+	return fmt.Sprintf("COALESCE(%v, FALSE) AS %v", field, as)
+}
+
+// TimeAS return converted SQL chunk for a nullable datetime typed field,
+// and this function is database dependent
+func TimeAs(database, field, as string) string {
+	switch database {
+	case "mysql":
+		return fmt.Sprintf("COALESCE(%v, CONVERT_TZ('0001-01-01 00:00:00','+00:00','UTC')) AS %v", field, as)
+	case "postgres":
+		return fmt.Sprintf("COALESCE(%v, (TIMESTAMP WITH TIME ZONE '0001-01-01 00:00:00+00') AT TIME ZONE 'UTC') AS %v", field, as)
+	}
+	return ""
+}
